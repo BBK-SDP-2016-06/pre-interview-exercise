fac89b2643f719da2493dc7081e4784b65c524ce
George Shiangoli <gshian01@mail.bbk.ac.uk>
2018-05-21 22:54:26 +0100

    All offer methods completed.

--- src/CurrentOffers.java
+++ src/CurrentOffers.java
@@ -12,5 +12,13 @@ public class CurrentOffers {
         };
     }
 
-
+    public static Offer ThreeForThePriceOfTwo(Fruit fruit) {
+        return (items) -> {
+            int offerItemsCount = (int)items.stream()
+                    .filter(i -> i.getClass().equals(fruit.getClass()))
+                    .count();
+            int discountItemsCount = offerItemsCount / 3;
+            return fruit.GetPrice().multiply(new BigDecimal(Integer.toString(discountItemsCount)));
+        };
+    }
 }
