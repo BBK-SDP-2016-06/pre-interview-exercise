ac308b0687eb9f20accfe9bcf812554794a8c93a
George Shiangoli <gshian01@mail.bbk.ac.uk>
2018-05-21 21:42:24 +0100

    Checkout with null items list now returns 0. Depending on business logic, a null reference or illegal argument exception can be thrown instead.

--- src/ShoppingCart.java
+++ src/ShoppingCart.java
@@ -1,11 +1,18 @@
+import org.junit.platform.commons.util.CollectionUtils;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 public class ShoppingCart {
 
     public BigDecimal Checkout(List<Fruit> items) {
-        return items.stream()
-                .map(Fruit::GetPrice)
-                .reduce(new BigDecimal("0"), BigDecimal::add);
+        if (items == null) {
+            return new BigDecimal("0");
+        }
+        else {
+            return items.stream()
+                    .map(Fruit::GetPrice)
+                    .reduce(new BigDecimal("0"), BigDecimal::add);
+        }
     }
 }
